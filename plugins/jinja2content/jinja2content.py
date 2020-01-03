5257422927fa2054eec4ad0961e6ebcd5f8fa6bc
epsi sayidina <epsi.rns@gmail.com>
2020-01-03 17:21:19 +0700

    Python 2.7 compatibility: super

--- plugins/jinja2content/jinja2content.py
+++ plugins/jinja2content/jinja2content.py
@@ -17,7 +17,7 @@ __metaclass__ = type
 
 class JinjaContentMixin():
     def __init__(self, *args, **kwargs):
-        # Python 2.7: CRITICAL: TypeError: super() takes at least 1 argument (0 given)
+        # Python 2.7 in netlify: CRITICAL: TypeError: super() takes at least 1 argument (0 given)
         super().__init__(*args, **kwargs)
 
         # will look first in 'JINJA2CONTENT_TEMPLATES', by default the
