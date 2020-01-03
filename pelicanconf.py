7ce6b6759ebcfd1a6b37649304107ed7eda18964
epsi sayidina <epsi.rns@gmail.com>
2020-01-03 15:14:14 +0700

    sys.path by Leksono Nanto

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,15 +3,14 @@
 from __future__ import unicode_literals
 from datetime import date
 
-# import sys
-# sys.path.append('.')
+# Fix netlify path, credit to Leksono Nanto
 
 import os, sys
 
 path_this = os.path.dirname(os.path.abspath(__file__))
-path_lib = os.path.abspath(os.path.join(path_this, 'lib'))
-
 sys.path.append(path_this)
+
+# -- -- -- Pelican Configuration Starts Here
 
 AUTHOR = 'epsi'
 SITENAME = 'Yet Another Static Blog'
@@ -63,6 +62,8 @@ AUTHOR_SAVE_AS        = 'author/{slug}.html'
 
 CURRENTYEAR = date.today().year
 
+# -- -- -- Pelican Configuration End Here
+
 # Plugins
 
 # from plugins.jinja2content import jinja2content
@@ -74,53 +75,6 @@ CURRENTYEAR = date.today().year
 
 # Filter
 # https://linkpeek.com/blog/how-to-add-a-custom-jinja-filter-to-pelican.html
-
-# https://stackoverflow.com/questions/31607710/randomize-elements-of-a-list-in-jinja-2
-
-import random
-
-def filter_shuffle(seq):
-  try:
-    result = list(seq)
-    random.shuffle(result)
-    return result
-  except:
-    return seq
-
-def filter_split(text, separator):
-  return text.split(separator)
-
-def filter_navigation(articles_by_dates, article):
-  result = {'has_prev': False, 'has_next': False, 'id': -1}
-
-  for index, post in enumerate(articles_by_dates):
-    if post.url == article.url: result['id'] = index
-
-  if result['id'] + 1 > 1:
-    result['has_prev'] = True
-
-    for index, post in enumerate(articles_by_dates):
-      if index == result['id'] - 1:
-        result['prev_url']   = post.url
-        result['prev_title'] = post.title
-
-  if result['id'] + 1 < len(articles_by_dates):
-    result['has_next'] = True
-
-    for index, post in enumerate(articles_by_dates):
-      if index == result['id'] + 1:
-        result['next_url']   = post.url
-        result['next_title'] = post.title
-
-  return result
-
-def filter_keyjoin(tags, category, keywords):
-  terms = []
-  terms.append(category)
-  terms.extend(tags)
-  terms.extend(keywords)
-  return terms
-
 
 # import lib.libfilter
 JINJA_FILTERS = {
@@ -135,15 +89,10 @@ JINJA_FILTERS = {
 # CRITICAL: ImportError: No module named lib.friends
 
 # Blogroll: Helper for friends widget
-# from lib.friends import *
-# from lib.archives_gitlab import *
-# from lib.archives_github import * 
-# from lib.archives_pelican import *
-
-# failed:  from . import friends
-# failed: FRIENDS = lib.friends.FRIENDS
-
-from friends import *
+from lib.friends import *
+from lib.archives_gitlab import *
+from lib.archives_github import * 
+from lib.archives_pelican import *
 
 # Opengraph
 OG_LOCALE = "en_US"
